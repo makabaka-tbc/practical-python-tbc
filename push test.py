87d03482ca10096166d97fdd8a482e8a870d9de2
mingyu.li <makabakazhishen@gmail.com>
2025-02-14 15:58:42 +0100

    Initial commit

--- push test.py
+++ push test.py
@@ -1,7 +1,12 @@
 import random
+from KevenGeiGei666 import countdayofyear
+
 ran = random.randint(1, 100)
 if __name__ == "__main__":
     print(f"test go to {ran}")
+    print(countdayofyear(input(), input(), input()))
+
 #  git commit -a
+#  esc (then ":wq")
 #  git push origin master
 #  git add .
